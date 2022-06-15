1adb1bb05bc4c02eb85c90ab86e71ad5407bb22d
ArlandMv <arlandmichelenav@gmail.com>
2022-06-14 22:55:04 -0400

    feat: initial frontend folder structure

---
+++ frontend/src/container/index.js
@@ -0,0 +1,23 @@
+/* export { default as About } from './About/About'
+export { default as Footer} from './Footer/Footer'
+export { default as Header } from './Header/Header'
+ */
+
+
+import About from './About/About';
+import Footer from './Footer/Footer';
+import Header from './Header/Header';
+import Skills from './Skills/Skills';
+import Testimonial from './Testimonial/Testimonial';
+import Work from './Work/Work';
+
+export {
+  About,
+  Footer,
+  Header,
+  Skills,
+  Testimonial,
+  Work,
+};
+
+
