eb93c51e58f6b391b200de608d2f5107711c014b
ArlandMv <arlandmichelenav@gmail.com>
2022-06-17 21:45:01 -0400

    feat: add implementation of sanity.io cms functionality

--- frontend/src/container/About/About.jsx
+++ frontend/src/container/About/About.jsx
@@ -1,9 +1,58 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { motion } from 'framer-motion';
+import { images } from '../../constants';
 
-//import './About.scss';
+import './About.scss';
+import { urlFor, client } from '../../client';
 
-function About() {
-	return <div>About</div>;
-}
+/* const abouts = [
+	{
+		title: 'Analísis Seo',
+		description: 'I check if you have done SEO ',
+		imgUrl: images.about01,
+	},
+	{ title: 'Projecto Seo', description: 'I do SEO', imgUrl: images.about02 },
+	{
+		title: 'Seo Consult',
+		description: 'I tell you how to SEO',
+		imgUrl: images.about03,
+	},
+]; */
+
+const About = () => {
+	const [abouts, setAbouts] = useState([]);
+
+	useEffect(() => {
+		const query = '*[_type == "abouts"]';
+		client.fetch(query).then((data) => setAbouts(data));
+	}, []);
+
+	return (
+		<>
+			<h2 className="head-text">
+				So che un <span>buon SEO</span>
+				<br /> significa <span>buoni affari</span>
+			</h2>
+
+			<div className="app__profiles">
+				{abouts.map((about, index) => (
+					<motion.div
+						whileInView={{ opacity: 1 }}
+						whileHover={{ scale: 1.1 }}
+						transition={{ duration: 0.5, type: 'tween' }}
+						className="app__profile-item">
+						<img src={urlFor(about.imgUrl)} alt={about.item} />
+						<h2 className="bold-text" style={{ marginTop: 20 }}>
+							{about.title}
+						</h2>
+						<p className="p-text" style={{ marginTop: 10 }}>
+							{about.description}
+						</p>
+					</motion.div>
+				))}
+			</div>
+		</>
+	);
+};
 
 export default About;
--- frontend/src/container/Header/Header.jsx
+++ frontend/src/container/Header/Header.jsx
@@ -1,9 +1,44 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 
-//import './Header.scss';
+import { images } from '../../constants';
+import './Header.scss';
 
-function Header() {
-	return <div>Header</div>;
-}
+const Header = () => {
+	return (
+		<div className="app__header app__flex">
+			<motion.div
+				whileInView={{ x: [-100, 0], opacity: [0, 1] }}
+				transition={{ duration: 0.5 }}
+				className="app__header-info">
+				<div className="app__header-badge">
+					<div className="badge-cmp app__flex">
+						<div style={{ marginLeft: 20 }}>
+							<p className="p-text">Ciao, Sono </p>
+							<p className="head-text">Beatriz Marín</p>
+						</div>
+					</div>
+					<div className="tag-cmp app__flex">
+						<p className="p-text">Seo Specialist</p>
+						<p className="p-text">& Data Analist</p>
+					</div>
+				</div>
+			</motion.div>
+			<motion.div
+				whileInView={{ opacity: [0, 1] }}
+				transition={{ duration: 0.5, delayChildren: 0.5 }}
+				className="app__header-img">
+				<img src={images.profile1} alt="profile_bg" />
+				<motion.img
+					whileInView={{ scale: [0, 1] }}
+					transition={{ duration: 1.2, ease: 'easeInOut' }}
+					className="overlay_circle"
+					src={images.circle}
+					alt="profile-circle"
+				/>
+			</motion.div>
+		</div>
+	);
+};
 
 export default Header;
